36f30eb0500d8e957be6c1be404db2fc347ef9f2
gaus <nadafgaus1402@gmail.com>
2026-03-30 16:27:38 +0530

    added postgrsl password

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from apps.orders.models import *
 # Register your models here.
+
+admin.site.register(OrderAcknowledgement)
+admin.site.register(Order)
+admin.site.register(OACommercialTerms)
